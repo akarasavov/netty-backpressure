000e34e4612225e938252c179231a4b6b2be0902
Aleksey Karasavov <aleksey.karasavov@neotechnology.com>
2021-01-07 16:11:25 +0000

    change the directory name

--- src/main/java/nioecho/EchoServer.java
+++ src/main/java/nioecho/EchoServer.java
@@ -13,12 +13,11 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package echo;
+package nioecho;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
